ecf3df83dd6f3b46577f3a30598fa06e14570fe9
believeyinuo <believeyinuo@163.com>
2019-12-18 22:13:24 +0800

    单例配置

--- src/test/java/TestGetToken.java
+++ src/test/java/TestGetToken.java
@@ -8,8 +8,8 @@ public class TestGetToken {
     @Test
     void testToken() {
         RestAssured.given().log().all()
-                .queryParam("corpid", "wwd6da61649bd66fea")
-                .queryParam("corpsecret", "1JPyY9GvPLZfpvxEDjok-Xt_9v7HIBYJhZUo06EgNGY")
+                .queryParam("corpid", WeworkConfig.getInstance().corpid)
+                .queryParam("corpsecret", WeworkConfig.getInstance().secret)
                 .when().get("https://qyapi.weixin.qq.com/cgi-bin/gettoken")
                 .then().statusCode(200).body("errcode", equalTo(0));
     }
